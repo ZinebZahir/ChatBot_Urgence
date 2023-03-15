386adde8ac5e1d902ac7d8c2e493f4abcb5c2f38
ZinebZahir <126154937+ZinebZahir@users.noreply.github.com>
2023-03-15 19:12:24 +0100

    Add files via upload

---
+++ ChatBot-urgence/static/scripts/responses.js
@@ -0,0 +1,184 @@
+function getBotResponse(input) {
+    if (input == "hello") {
+        return "hello, I am an urgence chatbot who can help you in you diffuclt situation, how can I help you";
+    } else if (input == "hey") {
+        return "hello, I am an urgence chatbot who can help you in you diffuclt situation, how can I help you";
+    } else if (input == "hy") {
+        return "hello, I am an urgence chatbot who can help you in you diffuclt situation, how can I help you";
+    } else if (input == "who are you") {
+        return "hello, I am an urgence chatbot who can help you in you diffuclt situation, how can I help you";
+    }
+
+    // english
+    if (input == "help") {
+        return "I'am here to help you, what do you need?";
+    }  else if (input == "help me") {
+        return "I'am here to help you, what do you need?";
+    }  else if (input == "police") {
+        return "call the number 19, is everythings okey?";
+    } else if (input == "firefighters") {
+        return "call the number 15, is everythings okey?";
+    } else if (input == "claiming number") {
+        return "call the number 13, is everythings okey?";
+    }else if (input == "SOS Medecins Maroc") {
+        return "call the number 05.22.98.98.98, is everythings okey?";
+    } else if (input == "Gendarmerie Royale") {
+        return "call the number 177, is everythings okey?";
+    } else if (input == "Maroc Assistance") {
+        return "call the number 05.22.30.30.30, is everythings okey?";
+    }  else if (input == "Corruption") {
+        return "call the number 08.00.100.76.76, is everythings okey?";
+    } else if (input == "ONCF") {
+        return "call the number 08.09.20.30.40, do you need something else?";
+    }  else if (input == "CTM") {
+        return "call the number 05.22.45.88.85, do you need something else?";
+    } else if (input == "need your help") {
+        return "how can I help you?";
+    } else if (input == "help me") {
+        return "how can I help you?";
+    } else if (input == "Emergency") {
+        return "What do you need?";
+    } else if (input == "I have a problem") {
+        return " what's your problem?";
+    } else if (input == "I'm stuck, I don't know what to do, Can you assist me?") {
+        return "what's your problem?";
+    } else if (input == "Urgent") {
+        return "what's your problem?";
+     }  else if (input == "what is your job") {
+        return "you can ask questions related to your safety in an emergency and I will answer. If you don't get the answer you want, you can keep asking questions and try to simplify your questions, as I deal with emergencies where questions are usually quick and easy. ";
+    }else if (input == "how can i use you") {
+        return "you can ask questions related to your safety in an emergency and I will answer. If you don't get the answer you want, you can keep asking questions and try to simplify your questions, as I deal with emergencies where questions are usually quick and easy. ";
+    }else if (input == "i'm afraid") {
+        return "it is normal to be afraid, because it can be a very frightening situation. try to keep calm and breathe deeply. how can I help you";
+    }else if (input == "how do you check a victim's breathing") {
+        return "Watch if the stomach and chest rise and feel air on the exhale.call emergency services if needed";
+    } else if (input == "how to stop a hemorrhage") {
+        return "Pressing hard on the bleeding site with your fingers or with the palm of the hand by putting a clean tissue on the wound.";
+    } else if (input == "What information should be transmitted to the services of help") {
+        return "The telephone number from which the call is made, the nature and the most precise location of the problem.";
+    }  else if (input == "I feel like there is an earthquake what should i do") {
+        return "Stay calm and focused, are you inside or outside";
+    } else if (input == "inside") {
+        return "1-Take shelter under heavy furniture, such as a table, desk, bed or any solid piece of furniture. If possible, move to the center of the room, away from windows. 2-Cover your head and neck with your arms to protect yourself from any objects that may fall on you. 3-Hold on to the object under which you have taken refuge to stay covered until the end of the tremors. 4-Is there no solid furniture under which to take refuge or are you in a corridor? Get into a squat position along an inside wall.  ";
+    } else if (input == "outside") {
+        return "1-Go to an open area, away from buildings.2-Are you in a busy public place? Take refuge in a safe place.3-Move away from power lines, trees, signs, buildings, vehicles and any other object that could fall and injure you.";
+    } else if (input == "I'm in an earthquake and I don't know what to do") {
+        return "Stay calm and focused, are you inside or outside";
+    }else if (input == "") {
+        return "";
+    }else if (input == "") {
+        return "";
+    }else if (input == "") {
+        return "";
+    }else if (input == "") {
+        return "";
+    }else if (input == "") {
+        return "";
+    }
+    
+    
+    //francais
+    
+     else if (input == "je suis dans une situation dangereuse") {
+        return "Pouvez-vous me dire où vous êtes et quel est le danger?";
+    } else if (input == "Je suis perdu en foret") {
+        return "Votre sécurité est notre priorité absolue. Pouvez-vous me donner votre position GPS pour que je puisse vous aider à trouver votre chemin?";
+    } else if (input == "gps introuvable") {
+        return "Si vous etes au proximité du rivière, essayez de la suivre. Les cours d'eau sont souvent reliés à des routes ou à des chemins. Si vous pouvez trouver une route, cela pourrait vous aider à sortir de la forêt.";
+    } else if (input == "gps introuvable") {
+        return "Si vous etes au proximité du rivière, essayez de la suivre. Les cours d'eau sont souvent reliés à des routes ou à des chemins. Si vous pouvez trouver une route, cela pourrait vous aider à sortir de la forêt.";
+    } else if (input == "salut") {
+        return "bonjour, je suis un chatbot d'urgence qui peut vous aider dans votre situation d'urgence, comment puis-je vous aider";
+    } else if (input == "bonjour") {
+        return "bonjour, je suis un chatbot d'urgence qui peut vous aider dans votre situation d'urgence, comment puis-je vous aider";
+    } else if (input == "comment vas-tu") {
+        return "bonjour, je suis un chatbot d'urgence qui peut vous aider dans votre situation d'urgence, comment puis-je vous aider";
+    }else if(input == "cv") {
+        return "bonjour, je suis un chatbot d'urgence qui peut vous aider dans votre situation d'urgence, comment puis-je vous aider";
+    } else if (input == "bon après-midi") {
+        return "bonjour, je suis un chatbot d'urgence qui peut vous aider dans votre situation d'urgence, comment puis-je vous aider";
+    } else if (input == "Accident") {
+        return "Je suis là pour vous aider, de quoi avez-vous besoin";
+    } else if (input == "Urgence médicale") {
+        return "Je suis là pour vous aider, de quoi avez-vous besoin";
+    } else if (input == "J'ai besoin d'aide") {
+        return "Je suis là pour vous aider, de quoi avez-vous besoin";
+    } else if (input == "Aidez-moi") {
+        return "Je suis là pour vous aider, de quoi avez-vous besoin";
+    } else if (input == "S'il vous plaît, aidez-moi") {
+        return "Je suis là pour vous aider, de quoi avez-vous besoin";
+    } else if (input == "Urgent !") {
+        return "Je suis là pour vous aider, de quoi avez-vous besoin";
+    } else if (input == "Pouvez-vous m aider") {
+        return "Je suis là pour vous aider, de quoi avez-vous besoin";
+    } else if (input == "J'ai besoin d'aide") {
+        return "Je suis désolé d'entendre cela ,Je suis là pour vous aider, de quoi avez-vous besoin";
+    } else if (input == "Je suis bloqué") {
+        return "Je suis là pour vous aider, de quoi avez-vous besoin";
+    } else if (input == "Je ne sais pas quoi faire") {
+        return "Je suis là pour vous aider, de quoi avez-vous besoin";
+    } else if (input == "police") {
+        return "appelez le 19";
+    } else if (input == "je veux appeler la police") {
+        return "voici le numéro de la police : 19, tout va bien ?";
+    } else if (input == "j'ai besoin du numéro de la police") {
+        return "voici le numéro de la police : 19, tout va bien ?";
+    } else if (input == "pompiers") {
+        return "appelez le 15";
+    } else if (input == "je veux appeler les pompiers") {
+        return "voici le numéro des pompiers : 15, tout va bien ?";
+    } else if (input == "j'ai besoin du numéro des pompiers") {
+        return "voici le numéro des pompiers : 15, tout va bien ?";
+    }else if (input == "SOS Medecins Maroc") {
+        return "appeler le numero 05.22.98.98.98, est-ce que tout va bien ?";
+    } else if (input == "Gendarmerie Royale") {
+        return "appeler le numero 177, est-ce que tout va bien ?";
+    } else if (input == "Maroc Assistance") {
+        return "appeler le numero 05.22.30.30.30,est-ce que tout va bien ?";
+    }  else if (input == "Corruption") {
+        return "appeler le numero 08.00.100.76.76, est-ce que tout va bien ?";
+    } else if (input == "ONCF") {
+        return "appeler le numero 08.09.20.30.40, avez-vous besoin d'autre chose?";
+    }  else if (input == "CTM") {
+        return "appeler le numero 05.22.45.88.85,avez-vous besoin d'autre chose?";
+    }   
+    else if (input == "comment ça marche") {
+        return "Vous pouvez poser des questions relatives à votre sécurité en cas d'urgence et je répondrai. si vous n'obtenez pas la réponse que vous désirez, vous pouvez continuer à poser des questions et essayer de simplifier vos questions, car je traite des situations d'urgence où les questions sont généralement simples et rapides. ";
+    } else if (input == "qu'est ce que tu fait") {
+        return "Vous pouvez poser des questions relatives à votre sécurité en cas d'urgence et je répondrai. si vous n'obtenez pas la réponse que vous désirez, vous pouvez continuer à poser des questions et essayer de simplifier vos questions, car je traite des situations d'urgence où les questions sont généralement simples et rapides.";
+    } else if (input == "j'ai peur") {
+        return "Il est normal d'avoir peur , car c'est une situation qui peut être très effrayante. Essayez de garder votre calme et de respirer profondément. comment puis-je t'aider ";
+    } else if (input == "comment verifier la respiration d’une victime") {
+        return "Regarder si le ventre et la poitrine se soulèvent et sentir de l’air à l’expiration.appeler l'urgence si nécessaire ";
+    } else if (input == "comment arreter une hémorragie ") {
+        return "En appuyant fortement sur l’endroit qui saigne avec les doigts ou avec la paume de la main en mettant un tissu propre sur la plaie. ";
+    } else if (input == "quelles sont les informations à transmettre aux services de secours ") {
+        return "Le numéro de téléphone à partir duquel l’appel est émis, la nature et la localisation la plus précise du problème";
+    } else if (input == "dans quelle situation peut-on deplacer une victime") {
+        return "En présence d’un danger réel, immédiat, non contrôlable. Le déplacement de la victime doit rester exceptionnel ";
+    } else if (input == "j'ai l'impression q'il y a un tremblement de terre que dois-je faire") {
+        return "Restez calme et concentré, êtes-vous à l'intérieur ou à l'extérieur ?";
+    } else if (input == "a l'interieur") {
+        return "1-Abritez-vous sous un meuble lourd, comme une table, un bureau, un lit ou tout meuble solide. Si possible, déplacez-vous au centre de la pièce, loin des fenêtres.2-Couvrez votre tête et votre cou avec vos bras pour vous protéger de tout objet qui pourrait vous tomber dessus. 3-Agrippez-vous à l’objet sous lequel vous êtes réfugié pour rester couvert jusqu’à la fin des secousses.4-In’y a aucun meuble solide sous lequel vous réfugier ou vous êtes dans un couloir? Mettez-vous en position accroupie le long d’un mur intérieur.";
+    } else if (input == "a l'exterieur") {
+        return "1-Dirigez-vous vers un endroit dégagé, loin des immeubles.2-Vous êtes dans un endroit public achalandé? Réfugiez-vous dans un endroit sécuritaire.3-Éloignez-vous des lignes à haute tension, des arbres, des enseignes, des bâtiments, des véhicules et de tout autre objet qui pourrait tomber et vous blesser.";
+    } else if (input == "among us") {
+        return "sus!!! ._.";
+    } else if (input == "je pense q'il y a un tremblement de terre ") {
+        return "Restez calme et concentré, êtes-vous à l'intérieur ou à l'extérieur";
+    }else if (input == "je suis dans un tremblement de terre et je ne sais pas quoi faire") {
+        return "Restez calme et concentré, êtes-vous à l'intérieur ou à l'extérieur";
+    }else if (input == "") {
+        return "";
+    }else if (input == "") {
+        return "";
+    }else if (input == "") {
+        return "";
+    }
+
+    
+   
+    else {
+        return "Try asking something else!";
+    }
+}
